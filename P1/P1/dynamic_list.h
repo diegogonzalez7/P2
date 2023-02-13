27358381e9b633e163f118d2aba8c9e57ac12c76
diego-fgomez <124870483+diego-fgomez@users.noreply.github.com>
2023-02-13 18:00:43 +0100

    Add files via upload

---
+++ P1/P1/dynamic_list.h
@@ -0,0 +1,42 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: DIEGO FERNÁNDEZ GÓMEZ LOGIN 1: diego.fgomez
+ * AUTHOR 2: DIEGO GONZÁLEZ GONZÁLEZ LOGIN 2: diego.gonzalez7
+ * GROUP: 4.2
+ * DATE: ** / ** / **
+ */
+
+#ifndef DYNAMIC_LIST_H
+#define DYNAMIC_LIST_H
+
+#include "types.h"
+#define LNULL NULL
+
+typedef struct Node* tPosL;
+struct Node{
+    tItemL data;
+    tPosL next;
+};
+typedef tPosL tList;
+
+//GENERADORAS
+void createEmptyList(tList *L);
+bool isEmptyList(tList L);
+
+//OBSERVADORAS
+tPosL first(tList L);
+tPosL last(tList L);
+tPosL next(tPosL p, tList L);
+tPosL previous(tPosL p, tList L);
+tPosL findItem(tParticipantName d, tList L);
+tItemL getItem(tPosL p, tList L);
+
+//MODIFICADORAS
+bool insertItem(tItemL d, tPosL p, tList *L);
+void updateItem(tItemL d, tPosL p, tList *L);
+
+//DESTRUCTORAS
+void deleteAtPosition(tPosL p, tList *L);
+
+#endif
