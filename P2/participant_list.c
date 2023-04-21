934e1ae2ee91ed8c32414d1075994daae824914b
diegogonzalez7 <diego.gonzalez7@udc.es>
2023-04-21 09:26:39 +0200

    cambios hechos en participant_list para que no nos denuncien again

--- P2/participant_list.c
+++ P2/participant_list.c
@@ -35,10 +35,11 @@ tPosP nextP(tPosP p, tListP L){
 
 tPosP previousP(tPosP p, tListP L){
     tPosP q;
-    if(p == L) return NULLP;
-    else{
-        q=L;
-        while((q->next)!= p){
+    if (p == L) {
+        return NULLP;
+    } else {
+        q = L;
+        while ((q->next) != p){
             q = q->next;
         }
         return q;
@@ -53,25 +54,43 @@ bool createNode(tPosP *p){
 tPosP findPosition(tListP L, tItemP d){   //Funcion auxiliar para insertItem
     tPosP p;
     p = L;
-    while((p->next != NULLP) && (strcmp(p->next->data.participantName, d.participantName) < 0)){
+    while ((p->next != NULLP) && (strcmp(p->next->data.participantName, d.participantName) < 0)){
         p = p->next;
     }
     return p;
 }
 
+tPosP findItemP(tParticipantName i, tListP L){
+    tPosP q;
+    if (isEmptyListP(L)){   //Lista vacía, no existe el item
+        return NULLP;
+    } else {
+        for (q = L; (q != NULLP) && (strcmp(q->data.participantName, i) < 0); q = q->next);
+        if ((q != NULLP) && (strcmp(q->data.participantName, i) == 0)){ //importante que primero compruebe si es nulo
+            return q;
+        } else {
+            return NULLP;
+        }
+    }
+}
+
+tItemP getItemP(tPosP p, tListP L){
+    return (p->data);
+}
+
 bool insertItemP(tItemP d, tListP * L){
     tPosP q, p;
-    if(!createNode(&q)){
+    if (!createNode(&q)){        //Comprobamos que se pueda crear el nuevo item
         return false;
     }else{
         q -> data = d;
         q -> next = NULLP;
-        if(*L == NULLP){
+        if (*L == NULLP){        //Caso 1: insertar en lista vacía
             *L = q;
-        }else if(strcmp(d.participantName, (*L)->data.participantName) < 0){
+        } else if (strcmp(d.participantName, (*L)->data.participantName) < 0){   //Caso 2: insertar al principio
             q -> next = *L;
             *L = q;
-        }else{
+        } else {                //Caso 3: insertar en cualquier posición
             p = findPosition(*L, d);
             q->next = p->next;
             p->next = q;
@@ -80,41 +99,25 @@ bool insertItemP(tItemP d, tListP * L){
     }
 }
 
+void updateItemP(tItemP d, tPosP p, tListP *L){
+    p->data = d;
+}
+
 void deleteAtPositionP(tPosP p, tListP *L){
     tPosP q;
-    if(p == *L){*L = (*L)->next;}
-    else if(p->next==NULLP){
-        for(q = *L; q->next != p; q = q->next);
+    if (p == *L) {       //Caso 1: eliminar el primer elemento
+        *L = (*L)->next;
+    }
+    else if (p->next == NULLP){     //Caso 2: eliminar el último elemento
+        for (q = *L; q->next != p; q = q->next);
         q->next = NULLP;
     }
-    else{
+    else{       //Caso 3: eliminar cualquier posición
         q = p->next;
         p->data = q->data;
         p->next = q->next;
         p = q;
     }
     free(p);
-}
-
-tItemP getItemP(tPosP p, tListP L){
-    return(p->data);
-}
-
-void updateItemP(tItemP d, tPosP p, tListP *L){
-    p->data = d;
-}
-
-tPosP findItemP(tParticipantName i, tListP L){
-    tPosP q;
-    if(isEmptyListP(L)){
-        return NULLP;
-    }else{
-        for(q = L; (q != NULLP) && (strcmp(q->data.participantName, i) < 0); q = q->next);
-        if((q != NULLP) && (strcmp(q->data.participantName, i) == 0)){
-            return q;
-        }else{
-            return NULLP;
-        }
-    }
 }
 
